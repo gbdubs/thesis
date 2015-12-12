90a5aedf900d82c21f2252db6fd2888d7ee62852
Grady Ward <gward@brandeis.edu>
2015-12-12 17:32:39 -0500

    Fixed a logical bug: I was only considering present sort order when sorting my vectors in the verifer.  Very happy to have caught this mistake.  As it turns out, this didn't impact my results, which makes me both happy and a bit nervous.

--- Cardinality Analysis/Java/Verification/src/Verification/Verifier.java
+++ Cardinality Analysis/Java/Verification/src/Verification/Verifier.java
@@ -1,15 +1,21 @@
 package Verification;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import data.Data;
 
 public class Verifier {
 
-	private static final int MAX_BIT_LENGTH = 600 * 10 * 10;
+	private static final int MAX_BIT_LENGTH = 1000 * 10 * 10;
 	
 	public static int testEqualPaths(String gA, String gB) throws DimensionMismatchException{
 		Matrix A = new Matrix(Graph6.retrieveGraph(gA));
@@ -20,22 +26,71 @@ public class Verifier {
 		
 		int p = 1;
 		
-		if (!Matrix.sortedDiagonalEquals(A, B)){
-			return p;
+		Map<Integer, List<BigInteger>> vectorsA = new HashMap<Integer, List<BigInteger>>();
+		Map<Integer, List<BigInteger>> vectorsB = new HashMap<Integer, List<BigInteger>>();
+		
+		for (int i = 0; i < A.x; i++){
+			List<BigInteger> lbiA = new ArrayList<BigInteger>();
+			lbiA.add(runningA.values[i][i]);
+			vectorsA.put(i, lbiA);
+			List<BigInteger> lbiB = new ArrayList<BigInteger>();
+			lbiB.add(runningB.values[i][i]);
+			vectorsB.put(i, lbiB);
 		}
 		
 		while (runningA.nBits() < MAX_BIT_LENGTH){
-			if (!Matrix.sortedDiagonalEquals(runningA, runningB)){
-				return p;
-			}
 			runningA = runningA.multiply(A);
 			runningB = runningB.multiply(B);
-			// System.err.println("POWER = "+p+", BITS = "+runningA.nBits());
+			
+			for (int i = 0; i < A.x; i++){
+				vectorsA.get(i).add(runningA.values[i][i]);
+				vectorsB.get(i).add(runningB.values[i][i]);
+			}
+			
+			if (compare(vectorsA.values(), vectorsB.values()) != 0){
+				return p;
+			}
+			
 			p++;
 		}
 		
 		return -1;
 	}
+	
+	public static int compare(Collection<List<BigInteger>> a, Collection<List<BigInteger>> b){
+		List<List<BigInteger>> A = new ArrayList<List<BigInteger>>(a);
+		List<List<BigInteger>> B = new ArrayList<List<BigInteger>>(b);
+		ListComparator lc = new ListComparator();
+		Collections.sort(A, lc);
+		Collections.sort(B, lc);
+		for (int i = 0; i < A.size(); i++){
+			int comparison = lc.compare(A.get(i), B.get(i));
+			if (comparison != 0){
+				return comparison;
+			}
+		}
+		return 0;
+	}
+	
+	private static class ListComparator implements Comparator<List<BigInteger>>{
+
+		@Override
+		public int compare(List<BigInteger> o1, List<BigInteger> o2) {
+			int sizeComparison = Integer.compare(o1.size(), o2.size());
+			if (sizeComparison != 0){
+				return sizeComparison;
+			}
+			
+			for (int i = 0; i < o1.size(); i++){
+				int comparison = o1.get(i).compareTo(o2.get(i));
+				if (comparison != 0){
+					return comparison;
+				}
+			}
+			return 0;
+		}
+	}
+	
 	
 	private static List<Set<String>> generateCopathsSets(String[] graphs){
 		List<Set<String>> sets = new ArrayList<Set<String>>();
@@ -64,7 +119,6 @@ public class Verifier {
 		
 		return sets;
 	}
-	
 	
 	public static void main(String[] args){
 		for(String name : Data.all.keySet()){
