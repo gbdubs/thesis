2b53077db1273aaefdcc75c127bd1821060589e0
Grady Ward <gward@brandeis.edu>
2015-12-14 09:05:06 -0500

    Created and ran a visualization script in matlab which should allow me to more readily tell which graphs have non/anit/no trivial automorphisms.

--- Matlab/visualizePaths.m
+++ Matlab/visualizePaths.m
@@ -1,36 +1,49 @@
-function [ output_args ] = visualizePaths( A )
-sizes = size(A);
-v = sizes(1);
+function [ output_args ] = visualizePaths( graphs_name )
 
-temp = generateLabeledKs(A, v);
-actual = zeros(v, v+1, 'double');
-actual(:,1:v) = log(cast(temp(:,1:v),'double') + 1);
-actual = actual - repmat(mean(actual), v, 1);
+ graphs = evalin('base', graphs_name);
+    for i = 1 : size(graphs)
+        A = graph6(graphs(i,:));
+        
+        figure;
+        %ax1 = subplot(1,2,1);
+        %gplot(ax1, A);
 
-imagesc(actual);                                   %# Create a colored plot of the matrix values
-colormap(flipud(gray));                         %# Change the colormap to gray (so higher values are
-                                                %#   black and lower values are white)
+        %ax2 = subplot(1,2,2);
+        ax2 = subplot(1,1,1);
+        colormap(ax2, hsv);
 
-textStrings = num2str(actual(:),'%0.2f');          %# Create strings from the matrix values
-textStrings = strtrim(cellstr(textStrings));    %# Remove any space padding
-[x,y] = meshgrid(1:v+1);                        %# Create x and y coordinates for the strings
+        sizes = size(A);
+        v = sizes(1);
 
-hStrings = text(x(:),y(:),textStrings(:), 'HorizontalAlignment','center');
+        temp = generateLabeledKs(A, v);
+        labels = strcat(num2str(temp(:,v+1)), '-', char(64 + temp(:,v+1)));
+        actual = zeros(v, v-1, 'double');
+        actual(:,1:v-1) = log(cast(temp(:,2:v),'double') + 1);
+        actual = actual - repmat(mean(actual), v, 1);
 
-midValue = mean(get(gca,'CLim'));               %# Get the middle value of the color range
-textColors = repmat(mat(:) > midValue,1,3);     %# Choose white or black for the
-                                                %#   text color of the strings so
-                                                %#   they can be easily seen over
-                                                %#   the background color
-set(hStrings,{'Color'},num2cell(textColors,2)); %# Change the text colors
+        imagesc(actual);
+        colormap(flipud(gray));
 
+        textStrings = num2str(actual(:),'%0.2f');        
+        textStrings = strtrim(cellstr(textStrings));    
+        [x,y] = meshgrid(1:v-1, 1:v);                   
 
+        hStrings = text(x(:),y(:),textStrings(:), 'HorizontalAlignment','center');
 
-set(gca,'XTick',1:v,...                         %# Change the axes tick marks
-        'XTickLabel',1:v,...  %#   and tick labels
-        'YTick',char(65:65+v),...
-        'YTickLabel',1:v,...
-        'TickLength',[0 0]);
+        textColors = repmat(actual(:) > 0, 1, 3); 
+
+        set(hStrings,{'Color'},num2cell(textColors,2)); 
+
+        set(gca,'XTick',1:v-1,'XTickLabel',2:v, ...
+                'YTick',1:v, 'YTickLabel',labels, 'TickLength',[0 0]);
+        
+        
+        
+        
+        path = strcat('visualization/',graphs_name,'-',char(64+i),'-paths.png');
+        print(path,'-dpng');
+        close all;
+    end 
 
 end
 
