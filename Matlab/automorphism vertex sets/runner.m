49b2cfec4a03697e63be15d44ca30fc89e5f12c6
gradyberryward <gward@brandeis.edu>
2016-01-14 15:23:46 -0500

    Ran tests on all copaths graphs, found that Paths determines automorphism groups for each of them perfectly. I need to be very certian to codify exactly why my weakness assumptions are not only acceptable but can demonstrate their stronger assumptions.

---
+++ Matlab/automorphism vertex sets/runner.m
@@ -0,0 +1,15 @@
+graphSet = v10e22;
+
+errors = 0;
+for i = 1 : size(graphSet, 1)
+    g = graph6(graphSet(i,:));
+    result = verifyPathsDefinesAutomorphismGroups(g);
+    if (result == 1)
+        disp 'ALL GOOD!';
+    else
+        disp 'ERROR!ERROR!ERROR!ERROR!ERROR!ERROR!';
+        errors = errors + 1;
+    end
+end
+
+errors
--- Matlab/startup.m
+++ Matlab/startup.m
@@ -1,2 +1,4 @@
 addpath(genpath('.'));
-disp 'Added all current subdirectories to path.'
+disp 'Added all current subdirectories to path.';
+data;
+disp 'Loaded data for the v=10 co-paths graphs.';
