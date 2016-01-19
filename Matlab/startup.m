5acd5a5d63fcb24767500e05c9aca49ad2334596
gradyberryward <gward@brandeis.edu>
2016-01-19 08:28:33 -0500

    Reorganization: Created a data directory to store data and data collection routines, slightly modified the startup file, and placed the distribution examination scripts in proper places.

--- Matlab/startup.m
+++ Matlab/startup.m
@@ -1,10 +1,12 @@
 addpath(genpath('.'));
-disp 'Added all current subdirectories to path.';
+disp 'Added all subdirectories of this directory to the path.';
 copathsData;
 disp 'Loaded data for the v=10 co-paths graphs.';
+automorphData;
+disp 'Loaded data for the automorphism vertex group failure cases.';
 for i = 1 : 8
     loadData(i);
+    disp(['Loaded data for all graphs with v = ', i, '.']);
 end
-disp 'Loaded data for all graphs v in [1,8].';
 clear i;
 clear ans;
