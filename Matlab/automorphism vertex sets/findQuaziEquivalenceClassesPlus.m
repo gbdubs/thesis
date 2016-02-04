c3a55995a6807bc13c21927049dca35b87969714
Grady Ward <gward@brandeis.edu>
2016-02-04 17:55:26 -0500

    Created a QEC splitter based on paths++

---
+++ Matlab/automorphism vertex sets/findQuaziEquivalenceClassesPlus.m
@@ -0,0 +1,33 @@
+function [ output_args ] = findQuaziEquivalenceClassesPlus( G )
+
+    qec = findQuaziEquivalenceClasses(G);
+    pathsPlus = cell(size(qec));
+    
+    for i = 1 : size(qec, 2)
+        class = cell2mat(qec(i));
+        if size(class, 2) > 1
+            for j = 1 : size(class, 2)
+                pathsPlus(class(j)) = {findExcessPaths(G, class(j))};
+            end
+            toEqual = cell2mat(pathsPlus(class(1)));
+            equalClass = [];
+            unequalClass = [];
+            for j = 1 : size(class, 2)
+                isEqual = 1 - any(toEqual - cell2mat(pathsPlus(class(j))));
+                if (isEqual)
+                    equalClass = horzcat(equalClass, class(j));
+                else
+                    unequalClass = horzcat(unequalClass, class(j));
+                end
+            end
+            if (size(unequalClass, 2) > 0)
+                qec(i) = {equalClass};
+                qec = horzcat(qec(1 : i), {unequalClass}, qec(i+1:end));
+                disp('split classes');
+            end
+        end
+    end
+    
+    output_args = qec;
+end
+
