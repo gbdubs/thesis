66b7173a8488a41eb87a1eb7628f3b278396cd5b
Grady Ward <gward@brandeis.edu>
2016-02-08 11:25:22 -0500

    Made Time Estimator Perfect.

--- Matlab/utils/mine/estimateTimeRemaining.m
+++ Matlab/utils/mine/estimateTimeRemaining.m
@@ -1,15 +1,39 @@
 function estimateTimeRemaining( startTime, nDone, nTotal )
-    disp (['Completed ',num2str(nDone),' of ',num2str(nTotal),'.']);
+    
+    function [ explanation ] = explainTime(secs)
+        if (secs > 60)
+            mins = floor(secs / 60);
+            secs = mod(secs, 60);
+            if (mins > 60)
+                hrs = floor(mins / 60);
+                mins = mod(mins, 60);
+                if (hrs > 24)
+                    days = floor(hrs / 24);
+                    hrs = mod(days, 24);
+                    explanation = [num2str(days),' days, ',num2str(hrs),' hours, ',num2str(mins),' minutes, and ',num2str(secs),' seconds'];
+                else
+                     explanation = [num2str(hrs),' hours, ',num2str(mins),' minutes, and ',num2str(secs),' seconds'];
+                end
+            else
+                explanation = [num2str(mins),' minutes, and ',num2str(secs),' seconds'];
+            end
+        else 
+            explanation = [num2str(secs),' seconds'];
+        end
+    end
     
     elapsed = cputime - startTime;
     nRemaining = nTotal - nDone;
     estimateCpuRemaining = nRemaining * (elapsed) / nDone;
-    disp(['Estimated CPU Time Remaining ',num2str(ceil(estimateCpuRemaining)),' seconds.']);
+    secsRemaining = ceil(estimateCpuRemaining);
     
-    currentTime = datetime('now');
-    currentTime.Second = currentTime.Second + estimateCpuRemaining;
+    pointOfCompletion = datetime('now');
+    pointOfCompletion.Second = pointOfCompletion.Second + estimateCpuRemaining;
     
-    disp('Estimated Time of Completion:');
-    disp(currentTime);
+    disp(['Completed ',num2str(nDone),' of ',num2str(nTotal),'.']);
+    disp(['Running for ', explainTime(ceil(elapsed)),'.']);
+    disp(['Estimated Time Remaining ', explainTime(secsRemaining),'.']);
+    disp(['Estimated Time of Completion: ', datestr(pointOfCompletion)]);
+    disp(' ');
 end
 
