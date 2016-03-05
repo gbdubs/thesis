bef791cdfac81c9907fcb930dd5c7dcc9ba74864
gradyberryward <gward@brandeis.edu>
2016-03-05 13:38:48 -0500

    Fixed a small problem with my cannonical labeler. Glad to have it resolved.

--- Matlab/automorphism vertex sets/findQuaziEquivalenceClassesPlus.m
+++ Matlab/automorphism vertex sets/findQuaziEquivalenceClassesPlus.m
@@ -10,19 +10,30 @@ function [ output_args ] = findQuaziEquivalenceClassesPlus( G )
                 pathsPlus(class(j)) = {findExcessPathsFast(G, class(j))};
             end
             toEqual = cell2mat(pathsPlus(class(1)));
+            greaterThanClass = [];
             equalClass = [];
-            unequalClass = [];
+            lessThanClass = [];
             for j = 1 : size(class, 2)
-                isEqual = 1 - any(toEqual - cell2mat(pathsPlus(class(j))));
-                if (isEqual)
+                % COMPARE HERE!!!!!
+                diff = toEqual - cell2mat(pathsPlus(class(j)));
+                temp = find(diff);
+                % IS EQUAL
+                if numel(temp) == 0
                     equalClass = horzcat(equalClass, class(j));
-                else
-                    unequalClass = horzcat(unequalClass, class(j));
+                elseif sign(diff(temp(1))) == -1
+                    lessThanClass = horzcat(lessThanClass, class(j));
+                elseif sign(diff(temp(1))) == 1
+                    greaterThanClass = horzcat(greaterThanClass, class(j));
                 end
             end
-            if (size(unequalClass, 2) > 0)
-                qec(i) = {equalClass};
-                qec = horzcat(qec(1 : i), {unequalClass}, qec(i+1:end));
+            if numel(lessThanClass) > 0
+                if numel(lessThanClass) > 0
+                    qec = horzcat(qec(1:i-1), {lessThanClass}, {equalClass}, {greaterThanClass}, qec(i+1:end));
+                else 
+                    qec = horzcat(qec(1:i-1), {lessThanClass}, {equalClass}, qec(i+1:end));
+                end
+            elseif numel(greaterThanClass) > 0
+                qec = horzcat(qec(1:i-1), {equalClass}, {greaterThanClass}, qec(i+1:end));
             end
         end
     end
---
+++ Matlab/oneoffs/untitled19.m
@@ -0,0 +1,4 @@
+A = graph6('IBhPUNoNw');
+B = graph6('IBgieNoNw');
+EA = graph6Encode(cannonical(A));
+EB = graph6Encode(cannonical(B));
