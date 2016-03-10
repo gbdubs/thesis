e31d6c80ae3c667b0770619989cd5c10c6c4cf5d
Grady Ward <gward@brandeis.edu>
2016-03-10 10:36:04 -0500

    Re-cannonized graphs eight and smaller.

--- Matlab/startup.m
+++ Matlab/startup.m
@@ -1,15 +1,14 @@
 clear all;
+
 addpath(genpath('.'));
 disp 'Added all subdirectories of this directory to the path.';
+
 copathsData;
 disp 'Loaded data for the v=10 co-paths graphs.';
-% automorphData;
-% disp 'Loaded data for the automorphism vertex group failure cases.';
-for i = 1 : 8
-    loadData(i);
-    disp(['Loaded data for all graphs with v = ', num2str(i), '.']);
-end
+
+load('cannonical/data/graphsEightAndSmaller.mat');
 disp 'Loaded all cannonical representations of graphs of degree 8 and smaller.';
-load('/Users/Grady/Desktop/Thesis/Matlab/cannonical/data/graphsEightAndSmaller.mat')
+
 clear i;
+
 clear('ans');
