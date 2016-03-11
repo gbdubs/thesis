328b9414892a991ffd6075db93e8477fb4846f7e
Grady Ward <gward@brandeis.edu>
2016-03-11 15:03:05 -0500

    Finished the processing of the first large collection.  I need to have more data on the intermediate range (.17 - .83)

--- Matlab/oneoffs/untitled26.m
+++ Matlab/oneoffs/untitled26.m
@@ -1,8 +1,8 @@
 ps = [.05, .1, .15, .2, .25, .3, .4, .5, .6, .7, .75, .8, .85, .9, .95];
-ns = 4 : 8;
+ns = 4 : 10;
 IdealResult = loadStep(ns, ps, 'Ideal','ANA',10000);
 StandardResult = loadStep(ns, ps, 'Standard','ANA',10000);
-for i = 1 : 5
+for i = 1 : 7
     figure;
     iR = log(IdealResult(i,:));
     sR = log(StandardResult(i,:));
@@ -10,6 +10,6 @@ for i = 1 : 5
     assignin('base',['iR', num2str(i+3)],iR);
     assignin('base',['sR', num2str(i+3)],sR);
     title(['Log ANA Standard and Ideal, N=',num2str(i+3)]);
-    savefig(['alternative generator/analysis/',num2str(i+3)]);
+    savefig(['alternative generator/analysis/ANA/',num2str(i+3)]);
 end
 close all;
