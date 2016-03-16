6c0845ff827121b004ec694e7ad4c7e3db766b0c
Grady Ward <gward@brandeis.edu>
2016-03-16 10:53:40 -0400

    Analysis!  A new set of AVG(LOG(NA)) metrics. Categorized Metrics into apparent effects.

--- Matlab/alternative generator/analysis/AutoGraphingMetricScript.m
+++ Matlab/alternative generator/analysis/AutoGraphingMetricScript.m
@@ -3,12 +3,46 @@ close all;
 minV = 4;
 maxV = 10;
 
-metric = 'PDL';
+
+metrics = [
+    {'MLNA'}
+    {'ALNA'}
+    {'VLNA'}
+%     {'ADSM'}
+%     {'DSMV'}
+%     {'ADSMD'}
+%     {'DSMDV'}
+%     {'ADSMN'}
+%     {'DSMNV'}
+%     {'ADSMX'}
+%     {'DSMXV'}
+%     {'ADSV'}
+%     {'DSVV'}
+%     {'ADSR'}
+%     {'PQRA'}
+%     {'PQRB'}
+%     {'PQRC'}
+%     {'ODSPL'}
+%     {'NCP'}
+%     {'NUG'}
+%     {'ANR'}
+%     {'MNR'}
+%     {'SQNR'}
+%     {'ANCC'}
+%     {'NCCV'}
+%     {'PTRIL'}
+%     {'ANTRI'}
+%     {'ANQUAD'}
+%     {'SQNR'}
+]';
 
 doLog = 0;
 
-for v = minV : maxV
-    generateComparisonAccrossP(v, metric, doLog)
-    print(['alternative generator/analysis/auto output/',metric,'-',num2str(v)],'-dpng');
-    close all;
+for metric = metrics
+    metricName = cell2mat(metric);
+    for v = minV : maxV
+        generateComparisonAccrossP(v, metricName, doLog)
+        print(['alternative generator/analysis/auto output/',metricName,'-',num2str(v)],'-dpng');
+        close all;
+    end
 end
