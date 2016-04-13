4a94a321037dafc1d155b82fd7c894b4df86597e
Grady Ward <gward@brandeis.edu>
2016-04-13 17:58:43 -0400

    </MESS>

--- Matlab/oneoffs/untitled37.m
+++ Matlab/oneoffs/untitled37.m
@@ -1,16 +1,16 @@
-for trial = 1 : 30
-    disp(['Trial = ', num2str(trial)]);
-    for n = 4 : 10
-        disp(['N = ', num2str(n)]);
-        for p = .1 : .1 : .9
-            disp(['P = ', num2str(p)]);
-            createBuildingARandomGraphData(n,p,1000,trial);
-            createBuildingBRandomGraphData(n,p,1000,trial);
-            createVerificationRandomGraphData(n,p,1000,trial);
-            createWinnerRandomGraphData(n,p,1000,trial);
-        end
-    end
-end
+% for trial = 1 : 30
+%     disp(['Trial = ', num2str(trial)]);
+%     for n = 4 : 10
+%         disp(['N = ', num2str(n)]);
+%         for p = .1 : .1 : .9
+%             disp(['P = ', num2str(p)]);
+%             createBuildingARandomGraphData(n,p,1000,trial);
+%             createBuildingBRandomGraphData(n,p,1000,trial);
+%             createVerificationRandomGraphData(n,p,1000,trial);
+%             createWinnerRandomGraphData(n,p,1000,trial);
+%         end
+%     end
+% end
 
 performAllProcessingOnAlgorithm('BuildingA');
 performAllProcessingOnAlgorithm('BuildingB');
