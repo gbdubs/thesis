b9b612d011588affe5bcebadcdace111f89a31d5
Grady Ward <gward@brandeis.edu>
2016-04-18 10:17:54 -0400

    Close read of Cycles Chapters.

--- Matlab/alternative generator/current processing/masterBaseliner.m
+++ Matlab/alternative generator/current processing/masterBaseliner.m
@@ -1,15 +1,20 @@
 
-algorithms = {'MinusOneA';'MinusOneB';'MinusOneC'};
+algorithms = {'BuildingC';'BuildingD';'BuildingE'};
 
 metricVariables = {'ADSM';'DSMV';'ADSMD';'DSMDV';'ADSMN';'DSMNV';'ADSMX';'DSMXV';'ADSV';'DSVV';'ADSR';'PQRA';'PQRB';'PQRC';'ODSPL';'ANA';'NCP';'NR';'NE';'PCONN';'ANCC';'NCCV';'ADIAM';'DIAMV';'PTRIL';'ANTRI';'ANQUAD';'ALNA';'MLNA';'VLNA';'NUG';'ANR';'MNR';'SQNR';'PDL'};
 
+totalIterations = numel(metricVariables) * 7 * 9 * numel(algorithms);
+index = 0;
+progressbar;
 
 for i = 1 : numel(metricVariables)
     metric = metricVariables{i};
     disp(['Currently processing metric ', metric]);
     for n = 4 : 10
         for p = .1 : .1 : .9
+            progressbar(index / totalIterations);
             for j = 1 : numel(algorithms)
+                index = index + 1;
                 alg = algorithms{j};
                 [~, sampleN, sampleMean, sampleSD ] = findSampleStatistics( n, p, alg, metric, 1000 );
                 saveToBaseline(n, p, alg, metric, sampleN, sampleMean, sampleSD);
---
+++ Matlab/oneoffs/untitled40.m
@@ -0,0 +1,12 @@
+masterCalculator;
+masterCalculator;
+
+masterBaseliner;
+
+scoreAgainstBaseline('BuildingC');
+scoreAgainstBaseline('BuildingD');
+scoreAgainstBaseline('BuildingE');
+
+createAlgorithmScoreReport('BuildingC');
+createAlgorithmScoreReport('BuildingD');
+createAlgorithmScoreReport('BuildingE');
